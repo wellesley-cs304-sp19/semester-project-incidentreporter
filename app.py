745c7c3dcc8d614fedbc09a44974e91cc301f96e
Karina Lin <klin3@wellesley.edu>
2019-04-18 02:15:13 +0000

    fixing to pass userID to inbox

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def studentInbox():
     incidentsList = incidentReporter.getAllReportedStudent(conn, uid)
     print(incidentsList)
     userInfo = incidentReporter.getUserInformation(conn, uid)
-    return render_template('inbox.html', userInfo=userInfo, incidentsList=incidentsList)
+    return render_template('inbox.html', userInfo=userInfo, userID=uid, incidentsList=incidentsList)
     
     
 @app.route('/facstaffInbox/')
@@ -48,7 +48,7 @@ def facstaffInbox():
     incidentsList = incidentReporter.getAllReportedFacstaff(conn, uid)
     print(incidentsList)
     userInfo = incidentReporter.getUserInformation(conn, uid)
-    return render_template('inbox.html', userInfo=userInfo, incidentsList=incidentsList)
+    return render_template('inbox.html', userInfo=userInfo, userID=uid, incidentsList=incidentsList)
 
     
 @app.route('/adminInbox/')
@@ -57,58 +57,8 @@ def adminInbox():
     uid = session['UID']
     incidentsList = incidentReporter.getAllIncidents(conn)
     userInfo = incidentReporter.getUserInformation(conn, uid)
-    return render_template('inbox.html', userInfo=userInfo, incidentsList=incidentsList)
+    return render_template('inbox.html', userInfo=userInfo, userID=uid, incidentsList=incidentsList)
 
-
-# @app.route('/rateMovie/', methods=['GET','POST'])
-# def rate_movie(searchTerm=None, json=None):
-#     conn = incidentReporter.getConn('wmdb')
-    
-#     if searchTerm == None:
-#         movies = incidentReporter.getAllMovies(conn)
-#     elif searchTerm:
-#         movies = incidentReporter.getMovieByTitle(conn, searchTerm)
-    
-        
-    
-#     try:
-#         uid = session['UID']
-#     except:
-#         uid = None
-        
-#     if request.method == 'GET':
-#         return render_template('movie-list.html',
-#                           movieList=movies,
-#                           userID = uid)
-
-
-# @app.route('/updateRating/', methods=['POST'])
-# def updateRating():
-    
-#     if request.method == 'POST':
-#         try:
-#             uid = session['UID']
-            
-#         except:
-#             flash("you must be logged in to rate a movie")
-#             return redirect(request.referrer)
-            
-#         try:
-#             conn = incidentReporter.getConn('wmdb')
-#             tt = request.form.get('tt')
-#             rating = request.form.get('stars')
-            
-#             incidentReporter.updateUserRating(conn, uid, tt, rating)
-#             newAvg = incidentReporter.calculateAverage(conn, tt)
-            
-#             flash ("New average rating of " + str(newAvg['avg_rating']))
-#             return redirect(request.referrer)
-            
-#         except:
-#             flash("please try again later")
-#             return redirect(request.referrer)
-        
-        
           
 if __name__ == '__main__':
     app.debug = True
