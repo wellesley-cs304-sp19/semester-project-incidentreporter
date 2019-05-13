32a06d293d83d49121ae06d01df3677a50d8ca1e
Katherine Gao <kgao@wellesley.edu>
2019-05-13 15:49:18 +0000

    fixing requesting faculty info

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def incidentReport():
                                 facStaff = facStaff,
                                 userInfo = userInfo)
     else:
-        rName = request.form.get['faculty']
+        rName = request.form['faculty']
         aName = request.form['advocate']
         rID = incidentReporter.getIDFromName(conn, rName)
         aID = incidentReporter.getIDFromName(conn, aName)
